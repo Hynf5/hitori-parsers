4da57f09bd3a9ab1f80a05646af963a72668099f
Hitori <165667417+Hynf5@users.noreply.github.com>
2026-01-04 05:43:23 +0700

    Fix kiryuu (by @dragonx943)

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/natsu/id/Kiryuu.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/natsu/id/Kiryuu.kt
@@ -1,4 +1,4 @@
-package org.dokiteam.doki.parsers.site.natsu.id
+package org.koitharu.kotatsu.parsers.site.natsu.id
 
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
@@ -9,11 +9,6 @@ import org.koitharu.kotatsu.parsers.site.natsu.NatsuParser
 @MangaSourceParser("KIRYUU", "Kiryuu", "id")
 internal class Kiryuu(context: MangaLoaderContext) :
     NatsuParser(context, MangaParserSource.KIRYUU, pageSize = 24) {
-
     override val configKeyDomain = ConfigKey.Domain("kiryuu03.com")
-
-    override fun onCreateConfig(keys: MutableCollection<ConfigKey<*>>) {
-        super.onCreateConfig(keys)
-        keys.add(userAgentKey)
+	override val hxTrigger = "getChapterList"
     }
-}
